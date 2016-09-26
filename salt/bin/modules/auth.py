5bf399abf87ef0f1725d3b217cda78a850c93b39
Phil Hendren <phil@ksubaka.com>
2016-09-26 19:57:48 +0100

    Move stuff around a bit into module layout

---
+++ salt/bin/modules/auth.py
@@ -0,0 +1,18 @@
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+# Generates Salt API token and returns it
+def get_token(args):
+    if args.disable_ssl_verification is True:
+        requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+    else:
+        pass
+
+    request_body_data = {"username": {salt_api_user}, "password": {salt_api_password}, "eauth": "pam"}
+
+    try:
+        request_token = requests.post(salt_api_endpoint + "/login", data=request_body_data, verify=False)
+        api_token = request_token.json()['return'][0]['token']
+        return api_token
+    except:
+        return False
