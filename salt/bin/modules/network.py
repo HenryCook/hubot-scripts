4282ef8bc21ff9f0894aa3f3384f336bf200ef8b
Henry Cook <henry.cook@forward3d.com>
2016-09-27 16:56:45 +0100

    Adding requirements.txt

--- salt/bin/modules/network.py
+++ salt/bin/modules/network.py
@@ -8,6 +8,8 @@ def ping_minion(args, api_token):
     body = {"client": "local", "tgt": ping_tgt, "fun": "test.ping"}
     headers = {"Accept": "application/x-yaml", "X-Auth-Token": api_token}
 
+    print("Attempting to ping " + ping_tgt + " from the Salt Master")
+
     try:
         requests.post(environment.salt_api_endpoint, data=body, headers=headers, verify=False)
         print("The Salt Master was able to successfully ping " + ping_tgt)
