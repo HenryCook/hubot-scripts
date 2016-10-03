6d78a32ccf9d4ec9652350afe9e5839bc38b5900
Henry Cook <henry@havingatinker.uk>
2016-10-03 17:56:29 +0100

    Needed to implement the prompt via the coffeescript and now the python script

--- elasticsearch/bin/elasticsearch/es_tool.py
+++ elasticsearch/bin/elasticsearch/es_tool.py
@@ -30,14 +30,8 @@ def main():
         print('Attempting to list all indices in the ' + "'" + environment.elasticsearch_endpoint + "'" + " cluster")
         indices.list_all(conn)
     elif args.delete_index:
-        prompt = raw_input('Are you sure you wish to remove ' + "'" + args.delete_index + "'" + '?: Y/N ').lower()
-        if prompt == "y":
-            print('Attempting to remove the index ' + "'" + args.delete_index + "'" + ' from the ' + "'" + environment.elasticsearch_endpoint + "'" + ' cluster')
-            indices.delete(args, conn)
-        elif prompt == "n":
-            print('The index ' + "'" + args.delete_index + "'" + ' is NOT being removed')
-        else:
-            print('You need to select an option (Y/N)')
+        print('Attempting to remove the index ' + "'" + args.delete_index + "'" + ' from the ' + "'" + environment.elasticsearch_endpoint + "'" + ' cluster')
+        indices.delete(args, conn)
     else:
         sys.exit()
     pass
