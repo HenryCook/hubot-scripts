74988816069adb7b1993b1c89aa21489bd8ea750
Henry Cook <henry@havingatinker.uk>
2016-10-04 10:33:59 +0100

    Adding exception handling indices.py

--- bin/elasticsearch/modules/indices.py
+++ bin/elasticsearch/modules/indices.py
@@ -2,10 +2,16 @@ def delete(args, conn):
     # To delete an index specified
     index_to_remove = args.delete_index
 
-    conn.indices.delete(index=index_to_remove)
-    print("'" + index_to_remove + "'" + " has been removed")
+    try:
+        conn.indices.delete(index=index_to_remove)
+        print("'" + index_to_remove + "'" + " has been removed")
+    except:
+        print("Unable to delete the " + "'" + index_to_remove + "'" + " index")
 
 
 def list_all(conn):
-    for index in conn.indices.get('*'):
-        print(index)
+    try:
+        for index in conn.indices.get('*'):
+            print(index)
+    except:
+        print("Unable to list al indices")
