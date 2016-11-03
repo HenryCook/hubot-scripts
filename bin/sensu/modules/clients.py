7b9c53ad0cf78a8c25a1dce99e7221f4d6c8cba7
Henry Cook <henry@havingatinker.uk>
2016-11-03 14:53:36 +0000

    Refactoring to not use string concatentation

--- bin/sensu/modules/clients.py
+++ bin/sensu/modules/clients.py
@@ -18,7 +18,7 @@ def clients_info(args):
 def clients_remove(args):
     clients_remove_tgt = args.clients_remove
     try:
-        requests.delete("http://" + environment.sensu_endpoint + "/clients/" + clients_remove_tgt)
+        requests.delete(environment.sensu_endpoint + "/clients/" + clients_remove_tgt)
         print("Succesfully removed the client '{remove}' from Sensu").format(remove=clients_remove_tgt)
     except:
         print("Unable to remove the client '{remove}' from Sensu").format(remove=clients_remove_tgt)
